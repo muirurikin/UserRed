3a7098ed4ce13fc900f3621b300c98e4be5e3c42
muirurikin <muirurikin@gmail.com>
2018-11-27 22:41:07 +0300

    Add about & contact routes

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,14 @@ router.get('/', (req, res) => {
   res.send('Hello, Welcome to the index page');
 });
 
+router.get('/about', (req, res) => {
+  res.send('Hello, Welcome to the index page');
+});
+
+router.get('/contact', (req, res) => {
+  res.send('Hello, Welcome to the index page');
+});
+
 module.exports = {
   router,
   userRoutes
