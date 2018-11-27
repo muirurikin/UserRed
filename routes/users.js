bc65bda44f3b4a77b954f1e29def0eeabb3a9dc6
muirurikin <muirurikin@gmail.com>
2018-11-27 22:39:40 +0300

    Add RESTful routes

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,25 @@ router.get('/', (req, res) => {
   res.send('Hello Users');
 });
 
+router.get('/new', (req, res) => {
+  res.send('User Added');
+});
+
 router.post('/', (req, res) => {
   res.send('User Added');
 });
+
+router.get('/:id/edit', (req, res) => {
+  res.send('User Added');
+});
+
+router.put('/:id', (req, res) => {
+  res.send('User Added');
+});
+
+router.delete('/id', (req, res) => {
+  res.send('User Added');
+});
+
 
 module.exports = router;
